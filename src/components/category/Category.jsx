b0cfbad30aa0691d8b104eb5f6fe0913de8e5482
P-Tanjim <moksedurtanjim27@gmail.com>
2026-08-20 03:46:02 +0600

    add icons for mobile shortcut

---
+++ src/components/category/Category.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Category = () => {
+  return (
+    <div className='w-[95%] md:w-[80%] lg:w-[70%] mx-auto'>
+        
+    </div>
+  )
+}
+
+export default Category
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-      <div className='absolute -translate-x-1/2 left-1/2 right-1/2 bottom-5 mobile-nav w-[95%] py-2 px-6 rounded-full backdrop-blur-sm bg-primary/80 min-[1003px]:hidden'>
+      <div className='absolute -translate-x-1/2 left-1/2 right-1/2 bottom-5 mobile-nav w-[90%] py-2 px-6 rounded-full backdrop-blur-sm bg-primary/80 min-[1003px]:hidden'>
           <ul className='flex flex-row justify-between items-center'>
             <Link href={'/'}>
               <li className='px-2 py-2'><House size={28} color="#ffffff" /></li>
